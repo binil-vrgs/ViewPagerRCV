532bb0efccb66d35595a78844c009691b73b4f80
binil <binil.vrgs@gmail.com>
2017-12-28 18:08:31 +0530

    added sample project

--- app/src/main/java/com/dev/binil/viewpagerrcv/MainActivity.java
+++ app/src/main/java/com/dev/binil/viewpagerrcv/MainActivity.java
@@ -1,12 +1,15 @@
 package com.dev.binil.viewpagerrcv;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.dev.binil.viewpager.ViewPagerRCV;
@@ -21,14 +24,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setList();
+        setList2();
     }
 
     private void setList() {
         ViewPagerRCV recyclerView = findViewById(R.id.list);
-        recyclerView.setStartOffSet(Offset);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(new RCVAdapter());
+
+        recyclerView.addOnScrollChangeListener(new ViewPagerRCV.OnScrollChangeListener() {
+            @Override
+            public void onScrollEnd(int topPosition) {
+                System.out.println("ViewPagerRCVFragment.onScrollEnd." + topPosition);
+            }
+
+            @Override
+            public void onScroll(int topPosition, int visibleItemsCount, int ItemsCount, int direction) {
+                System.out.println("ViewPagerRCVFragment.onScroll" +
+                        "_Top:" + topPosition +
+                        "_VisibleItemCount:" + visibleItemsCount +
+                        "_itemsCount:" + ItemsCount +
+                        "_Direction:" + direction
+                );
+            }
+        });
+    }
+
+    private void setList2() {
+        ViewPagerRCV recyclerView = findViewById(R.id.list2);
+//        recyclerView.setStartOffSet(Offset);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new RCVAdapter2());
 
         recyclerView.addOnScrollChangeListener(new ViewPagerRCV.OnScrollChangeListener() {
             @Override
@@ -72,10 +100,40 @@ public class MainActivity extends AppCompatActivity {
             ViewHolder(View itemView) {
                 super(itemView);
 
-//                Display display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-//                ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
-//                layoutParams.width = display.getWidth() - Offset;
-//                itemView.setLayoutParams(layoutParams);
+                mName = itemView.findViewById(R.id.name);
+                mCount = itemView.findViewById(R.id.count);
+            }
+        }
+    }
+
+    private class RCVAdapter2 extends RecyclerView.Adapter<RCVAdapter2.ViewHolder> {
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item2, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.mName.setText("Item");
+            holder.mCount.setText(String.valueOf(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 200;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView mName, mCount;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+
+                Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+                ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
+                layoutParams.width = display.getWidth() - Offset;
+                itemView.setLayoutParams(layoutParams);
 
                 mName = itemView.findViewById(R.id.name);
                 mCount = itemView.findViewById(R.id.count);
