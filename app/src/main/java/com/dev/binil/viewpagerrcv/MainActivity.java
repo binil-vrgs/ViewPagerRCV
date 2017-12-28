6907290469cc95f1353cf714298024741e4a6185
binil <binil.vrgs@gmail.com>
2017-12-28 17:51:08 +0530

    ViewPagerRCV library is integrated.

--- app/src/main/java/com/dev/binil/viewpagerrcv/MainActivity.java
+++ app/src/main/java/com/dev/binil/viewpagerrcv/MainActivity.java
@@ -1,13 +1,85 @@
 package com.dev.binil.viewpagerrcv;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dev.binil.viewpager.ViewPagerRCV;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int Offset = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setList();
+    }
+
+    private void setList() {
+        ViewPagerRCV recyclerView = findViewById(R.id.list);
+        recyclerView.setStartOffSet(Offset);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new RCVAdapter());
+
+        recyclerView.addOnScrollChangeListener(new ViewPagerRCV.OnScrollChangeListener() {
+            @Override
+            public void onScrollEnd(int topPosition) {
+                System.out.println("ViewPagerRCVFragment.onScrollEnd." + topPosition);
+            }
+
+            @Override
+            public void onScroll(int topPosition, int visibleItemsCount, int ItemsCount, int direction) {
+                System.out.println("ViewPagerRCVFragment.onScroll" +
+                        "_Top:" + topPosition +
+                        "_VisibleItemCount:" + visibleItemsCount +
+                        "_itemsCount:" + ItemsCount +
+                        "_Direction:" + direction
+                );
+            }
+        });
+    }
+
+    private class RCVAdapter extends RecyclerView.Adapter<RCVAdapter.ViewHolder> {
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.mName.setText("Item");
+            holder.mCount.setText(String.valueOf(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 200;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView mName, mCount;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+
+//                Display display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+//                ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
+//                layoutParams.width = display.getWidth() - Offset;
+//                itemView.setLayoutParams(layoutParams);
+
+                mName = itemView.findViewById(R.id.name);
+                mCount = itemView.findViewById(R.id.count);
+            }
+        }
     }
 }
